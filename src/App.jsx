237153e785e695aeee94d68e1c97ce65317d832c
Gerson <espitiagerson236@gmail.com>
2026-07-24 12:50:08 -0500

    Tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import './App.css';
 import Tarjeta from './components/tarjeta';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App (){
   return(
     <div>
-      <Header/>
-      <main>
-        <Tarjeta />
-        <Tarjeta />
-        <Tarjeta />
-      </main>
-      <Footer/> 
+      <Navbar />
+      <Hero />
     </div>
   ); 
 };
